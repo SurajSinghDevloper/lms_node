4fd2d763249bcfd9cdc896ec9c4eea5d1382d71a
Suraj Kumar <surbhi.kumari246@gmail.com>
2024-11-26 10:06:20 +0530

    changes in staff panel admission section

--- src/main_routes/addmission_routes/staffs/addmissionRoutes.js
+++ src/main_routes/addmission_routes/staffs/addmissionRoutes.js
@@ -28,6 +28,25 @@ router.get('/allpending/std', authMiddleware, async (req, res) => {
         return res.status(500).json({ message: 'Internal Server Error' });
     }
 });
+router.get('/un-approved', authMiddleware, async (req, res) => {
+    try {
+        const email = req.query.email;
+        const result = await admissionServices.getDataOfUnAuthosrizedStudent(email);
 
+        // Check the status returned from the service
+        if (result.status === 204) {
+            return res.sendStatus(204); // No Content
+        }
+
+        // Handle the success case when a user is found (result.status === 200)
+        return res.status(200).json({
+            message: 'User data retrieved successfully.',
+            data: result.data // Return the found user data
+        });
+    } catch (error) {
+        console.error('Error during registration:', error);
+        return res.status(500).json({ message: 'An unexpected error occurred. Please try again later.' });
+    }
+});
 
 export default router;
