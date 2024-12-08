f0050ba5b5d2821a20deee3a9d4b04e90b45f567
Avinash <avinashverma078@gmail.com>
2024-12-08 06:45:00 +0530

    initial commit

--- server_app.js
+++ server_app.js
@@ -1,11 +1,10 @@
-import express from 'express';
-import cors from 'cors';
-import connectDB from './src/config/database.js';
-import { logger, morganMiddleware } from './src/config/logger.js';
-import routes from './src/main_routes/index.js';
-import { swaggerUi, swaggerSpec } from './src/config/swagger.js';
-import { startSignalingServer } from './src/vcall/signalingServer.js';
-
+import express from "express";
+import cors from "cors";
+import connectDB from "./src/config/database.js";
+import { logger, morganMiddleware } from "./src/config/logger.js";
+import routes from "./src/main_routes/index.js";
+import { swaggerUi, swaggerSpec } from "./src/config/swagger.js";
+import { startSignalingServer } from "./src/vcall/signalingServer.js";
 
 const app = express();
 app.use(express.json());
@@ -17,9 +16,9 @@ app.use(cors());
 // Connect to MongoDB
 connectDB();
 
-app.use('/api/v1/lmsb', routes);
+app.use("/api/v1/lmsb", routes);
 // Swagger route
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 startSignalingServer();
 
